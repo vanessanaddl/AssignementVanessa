e463431620231ac568f1260be81a76900e381869
vanessanaddl <vanessa.bohnet@web.de>
2016-02-28 10:15:15 +0000

    WHAT?! :D

--- AssignementVanessa/src/assignementVanessa/WordList.java
+++ AssignementVanessa/src/assignementVanessa/WordList.java
@@ -2,11 +2,23 @@ package assignementVanessa;
 import java.util.ArrayList;
 
 import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 @PersistenceCapable
 public class WordList {
-	ArrayList TestList = new ArrayList();
+	ArrayList <String> List = new ArrayList<String>();
+	@PrimaryKey
+	@Persistent
+	private Key key;
 	
+	WordList(Key keyCons)
+	{
+		key = keyCons; 
+	}
 
 
 }
